35035637431f780bf425eac15825ae80b746e233
lindomarfarineli <lindomarfarineli@hotmail.com>
2024-03-20 11:04:24 -0300

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,11 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',  
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'primeiro-projeto-latest';
 }
+
+//<RouterOutlet> <router-outlet/>
